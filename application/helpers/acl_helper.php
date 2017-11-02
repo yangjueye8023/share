66ff8d8b6f0f3eb3a3fdebf352d67096acb44d43
yangjueye8023 <33069667+yangjueye8023@users.noreply.github.com>
2017-11-02 19:33:49 +0800

    Update acl_helper.php

--- application/helpers/acl_helper.php
+++ application/helpers/acl_helper.php
@@ -2,7 +2,7 @@
 use \LeanCloud\User;
 
 function acl () {
-	if (User::getCurrentUser() != null && User::getCurrentUser()->get('username') == 'lendoo') {
+	if (User::getCurrentUser() != null) {
 		return true;
 	}
 	return false;
